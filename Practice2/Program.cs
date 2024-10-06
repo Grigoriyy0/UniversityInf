575a8461aa38b0f706a9facd5d73e4ad58415e42
Grigorii Kokotkin <padgamerrfp@gmail.com>
2024-10-07 00:22:51 +0400

    practice2

---
+++ Practice2/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace Practice2;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string input = @"C:\Practice2\Input.txt";
+        string output = @"C:\Practice2\Output.txt";
+
+        int numberOfTest = int.Parse(File.ReadLines(input).FirstOrDefault());
+        string[] data = File.ReadLines(input).Skip(1).ToArray();
+
+        for (int i = 0; i < numberOfTest; i++)
+        {
+            string nextTicket = GetNextTicket(data[i]);
+            string prevTicket = GetPrevTicket(data[i]);
+            if (IsHappy(prevTicket) || IsHappy(nextTicket))
+            {
+                using (StreamWriter writer = new StreamWriter(output))
+                {
+                    writer.WriteLine("Yes");
+                }
+            }
+            else
+            {
+                using (StreamWriter writer = new StreamWriter(output))
+                {
+                    writer.WriteLine("No");
+                }
+            }
+        }
+    }
+
+    public static int SumInString(string input)
+    {
+        int sum = 0;
+        foreach (char c in input)
+        {
+            sum += int.Parse(c.ToString());
+        }
+        return sum;
+    }
+
+    public static string GetPrevTicket(string ticket)
+    {
+        int num = int.Parse(ticket);
+        return (num-1).ToString();
+    }
+
+    public static string GetNextTicket(string ticket)
+    {
+        int num = int.Parse(ticket);
+        return (num + 1).ToString();
+    }
+    
+
+    public static bool IsHappy(string ticket)
+    {
+        int sumFirstThree = SumInString(ticket.Substring(0,3));
+        int sumLastThree = SumInString(ticket.Substring(3,3));
+
+        if (sumFirstThree == sumLastThree)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+            
+    } 
+}
