0a5a72f0af786595a643a6f14993d53ad751d0f8
Grigorii Kokotkin <46168908+Grigoriyy0@users.noreply.github.com>
2024-10-20 12:12:12 +0400

    ThirdPractice

---
+++ Practice3/Practice3/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Practice3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        var c1 = new Cicrcle(10);
+        c1.CalculateCircumference();
+    }
+}
+
+public class Cicrcle
+{
+    public double CircleRadius { get; set; }
+
+    public Cicrcle(double circleRadius)
+    {
+        CircleRadius = circleRadius;
+    }
+
+    public void CalculateCircumference()
+    {
+        Console.WriteLine(2*Math.PI*CircleRadius);
+    }
+}
